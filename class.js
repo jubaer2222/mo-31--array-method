5c401bd194df4c68b350534d56113ce271af290c
jubaer2222 <139365401+jubaer2222@users.noreply.github.com>
2025-07-22 12:31:32 +0600

    class in js

---
+++ class.js
@@ -0,0 +1,30 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hi, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+}
+
+
+const person1 = new Person("Jubaer", 25);
+person1.greet(); // Output: Hi, my name is Jubaer and I am 25 years old.
+
+
+class Student extends Person {
+  constructor(name, age, id) {
+    super(name, age); // parent class এর constructor call
+    this.id = id;
+  }
+
+  showID() {
+    console.log(`My ID is ${this.id}`);
+  }
+}
+
+const s1 = new Student("Arafat", 20, "12345");
+s1.greet();     // Parent method
+s1.showID();    // Child method
